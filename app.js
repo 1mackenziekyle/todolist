08cdbb616dba01e3afa513e1200839da4071f608
1mackenziekyle <1mackenziekyle@gmail.com>
2021-07-24 16:35:32 -0400

    Ready for deployment

--- app.js
+++ app.js
@@ -50,4 +50,4 @@ app.get("/about", (req,res) => {
     res.render("about");
 });
 
-app.listen(4000, () => console.log('App listening on port 4000.'));
+app.listen(process.env.PORT || 4000, () => console.log('App listening on port 4000.'));
