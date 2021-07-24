d6c71f8045985c9386655e54025a651c20631eb7
1mackenziekyle <1mackenziekyle@gmail.com>
2021-07-24 16:11:21 -0400

    V1: -/ -/work -/about

--- app.js
+++ app.js
@@ -7,36 +7,47 @@
 
 let express = require('express');
 let bodyParser = require('body-parser');
+let date = require(__dirname + "/date.js");
 let app = express();
 app.use(bodyParser({ extended: true } ) );
 app.use(express.static("public"));
-let items = [];
+let items = ['Pick up mail', 'Buy milk and eggs', 'work out 30 mins'];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
-/* Set the home page to display the date*/
+/*  MAIN PAGE */
 app.get('/', (req, res) => {
-    let today = new Date();
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    };
-    let date = today.toLocaleDateString("en-US", options);
+    let path = '/';
 
     /* Render list.ejs from views */
     res.render('list', {
-        kindOfDay: date,
+        path: path,
+        listTitle: date.getDate(),
         listItems: items
-    });
-});
+    } );
+} );
 
 app.post("/", (req, res) => { // each new input
     let item = req.body.newItem;
     items.push(item);
     res.redirect('back');
-});
+} );
 
+// WORK PAGE
+app.get('/work', (req,res) => {
+    let path = '/work';
+    res.render("list", {path: path, listItems: workItems, listTitle: "Work List"});
+    app.post("/work", (req,res) => {
+        let item = req.body.newItem;
+        workItems.push(item);
+        res.redirect('back');
+    } );
+} );
+
+// ABOUT PAGE
+app.get("/about", (req,res) => {
+    res.render("about");
+});
 
 app.listen(4000, () => console.log('App listening on port 4000.'));
