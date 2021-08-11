4c538d374e43b7076471d2abf581b6f786572582
1mackenziekyle <1mackenziekyle@gmail.com>
2021-08-11 12:27:10 -0400

    updated app.js & views/list.ejs to be congruent with v2

--- app.js
+++ app.js
@@ -1,52 +1,57 @@
-/* App.js Template
+/* 
+ * Website: https://serene-meadow-63566.herokuapp.com/  
+ * App.js Template
  *  - Requires express and body parser
  *  - Creates an instance of express called 'app'
  *  - Listens on Port 3000
  *  - Gets the root directory and sends 'hello' on the response
  */
 
+// Dependencies
 let express = require('express');
 let bodyParser = require('body-parser');
 let date = require(__dirname + "/date.js");
 let app = express();
-app.use(bodyParser({ extended: true } ) );
+app.set('view engine', 'ejs');
+app.use(bodyParser({
+    extended: true
+}));
 app.use(express.static("public"));
+
+// Items
 let items = ['Pick up mail', 'Buy milk and eggs', 'work out 30 mins'];
 let workItems = [];
 
-app.set('view engine', 'ejs');
+
 
 /*  MAIN PAGE */
 app.get('/', (req, res) => {
-    let path = '/';
-
     /* Render list.ejs from views */
     res.render('list', {
-        path: path,
         listTitle: date.getDate(),
         listItems: items
-    } );
-} );
+    });
+});
 
 app.post("/", (req, res) => { // each new input
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect('back');
-} );
 
-// WORK PAGE
-app.get('/work', (req,res) => {
-    let path = '/work';
-    res.render("list", {path: path, listItems: workItems, listTitle: "Work List"});
-    app.post("/work", (req,res) => {
-        let item = req.body.newItem;
+    if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect('back');
-    } );
-} );
+    } else {
+        items.push(item);
+        res.redirect('back');
+    }
+});
+
+// WORK PAGE
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+  });
 
 // ABOUT PAGE
-app.get("/about", (req,res) => {
+app.get("/about", (req, res) => {
     res.render("about");
 });
 
