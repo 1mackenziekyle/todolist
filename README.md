3d3e0e3a4f07c783ef6d0e655f6d8ef8c3b0a34a
1mackenziekyle <1mackenziekyle@gmail.com>
2021-08-16 15:37:19 -0400

    added mongoDB database.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 A simple todo list using Embedded JavaScript Templates
+https://serene-meadow-63566.herokuapp.com/
--- app.js
+++ app.js
@@ -12,9 +12,13 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 
-// Set up app
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true});
-let date = require(__dirname + "/date.js");
+
+// Connect to the mongoose database
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+    useNewUrlParser: true
+});
+
+// Normal express.js App setup
 let app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser({
@@ -23,46 +27,102 @@ app.use(bodyParser({
 app.use(express.static("public"));
 
 // Mongoose Schema
+const itemSchema = { name: String }
+const Item = mongoose.model('Item', itemSchema);
 
-const itemschema = {
-    name: String,
-    required: true
-}
-// Items
-let items = ['Pick up mail', 'Buy milk and eggs', 'work out 30 mins'];
-let workItems = [];
+// Default items
+const item1 = new Item({
+    name: "Work out"
+});
+const item2 = new Item({
+    name: "Buy food"
+});
+const item3 = new Item({
+    name: "Cook Food"
+});
+const defaultItems = [item1, item2, item3];
+
+// Add default items if todo list empty. Else, show length of list.
+Item.find({}, (err, foundItems) => {
+    /* .find function:
+     * Params:
+     * First parameter: the condition that each object found will satisfy. Called the 'filter', is a JS object.
+     * Optional" Fields to return, then options
+     * Final parameter: callback function which we pass the found items into, 
+    */
+    console.log("Length: ");
+    console.log(foundItems.length);
+    if (foundItems.length === 0) {
+        Item.insertMany(defaultItems, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Successfully added default items to database.");
+                res.redirect('/');
+            }
+        });
+    }
+});
+
 
 
 
 /*  MAIN PAGE */
 app.get('/', (req, res) => {
-    /* Render list.ejs from views */
-    res.render('list', {
-        listTitle: date.getDate(),
-        listItems: items
+    // Find all items
+    Item.find((err, items) => { // Item is the collection, items is the result
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("list", {
+                listTitle: "Today",
+                listItems: items,
+            });
+        }
     });
 });
 
 app.post("/", (req, res) => { // each new input
-    let item = req.body.newItem;
+    let itemName = req.body.newItem;
 
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect('back');
-    } else {
-        items.push(item);
-        res.redirect('back');
-    }
+    let item = new Item({
+        name: itemName
+    });
+
+    item.save();
+
+    res.redirect('/')
+    
 });
 
+
+// Delete route
+app.post("/delete", (req,res) => {
+    Item.findByIdAndRemove(req.body.checkbox, (err) => {
+        if (!err) {
+            console.log("Successfully deleted item: ");
+            console.log(req.body.checkbox);
+        }
+        else {
+            console.log(err);
+        }
+    });
+
+    res.redirect('/');
+} );
+
 // WORK PAGE
-app.get("/work", function(req,res){
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
-  });
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
 
 // ABOUT PAGE
 app.get("/about", (req, res) => {
     res.render("about");
 });
 
-app.listen(process.env.PORT || 4000, () => console.log('App listening on port 4000.'));
+// app.listen(process.env.PORT || 4000, () => console.log('App listening on port 4000.'));
+app.listen(4000, () => console.log('App listening on port 4000.'));
